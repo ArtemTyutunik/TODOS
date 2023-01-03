878e1b61b6f37fd1186cd42524d2a177c3e51e58
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-01-03 16:50:29 +0200

    enable logout

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APP_API_KEY,
-    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_APP_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_APP_ID,
-    measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-
 
 export const app = initializeApp(firebaseConfig);
--- src/widgets/header/ui/userSettingsModal.tsx
+++ src/widgets/header/ui/userSettingsModal.tsx
@@ -1,30 +1,55 @@
-import React from "react";
-import {IconButton, List, ListItem, ListItemButton,  ListItemIcon,  ListItemText,  Menu,  Tooltip,} from "@mui/material";
+import React, {FC, ReactNode} from "react";
+import {Avatar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, Tooltip, Typography,} from "@mui/material";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 
+import {useDispatch, useSelector} from "react-redux";
+import {RootReducer} from "../../../app/store";
+import {logOutUser} from "../../../pages/authorization/model";
+
 type menuItem = {
     label: string,
-    Icon: () => React.ReactElement
+    Icon: () => React.ReactElement,
+    onClick: () => void
 }
+
+interface  customListProps {
+    children: ReactNode[]
+}
+
+const CustomListItemButton:FC<customListProps> = ({children}) => {
+    return <ListItemButton sx = {{padding: '15px', borderRadius: "5px"}} className={"str"}>
+        {children}
+    </ListItemButton>
+}
+
 
 export default function UserSettingsMenu() {
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    const {email} = useSelector((state: RootReducer) => state.userReducer.user.user);
+    const dispatch = useDispatch();
+
     const  menuItems: menuItem[] = [
         {
             label: 'settings',
-            Icon: () => <SettingsIcon/>
+            Icon: () => <SettingsIcon/>,
+            onClick: () => {}
         },
         {
             label: 'theme',
-            Icon: () => <DarkModeIcon/>
+            Icon: () => <DarkModeIcon/>,
+            onClick: () => {}
         },
 
         {
             label: 'logout',
-            Icon: () => <LogoutIcon/>
+            Icon: () => <LogoutIcon/>,
+            onClick: () => {
+                dispatch(logOutUser());
+                localStorage.removeItem('user')
+            }
         }]
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -49,7 +74,7 @@ export default function UserSettingsMenu() {
                    </IconButton>
                </Tooltip>
                <Menu
-                   sx={{ mt: '45px' }}
+                   sx={{ mt: '45px'}}
                    id="menu-appbar"
                    anchorEl={anchorElUser}
                    anchorOrigin={{
@@ -64,19 +89,46 @@ export default function UserSettingsMenu() {
                    open={Boolean(anchorElUser)}
                    onClose={handleCloseUserMenu}
                >
+                   <ListItem sx = {{padding: 0}}>
+                           <Box width={'auto'}
+                                display={"flex"}
+                                padding = {"5px"}>
+                               <CustomListItemButton>
+                                   <Avatar sx={{
+                                       border:' 2px solid #48833f',
+                                       background: "transparent",
+                                       color: '#48833f',
+                                       marginRight: '30px'
+                                   }}>
+                                       A
+                                   </Avatar>
+                                   <Box>
+                                       <Typography fontWeight = {'bold'}>
+                                           {email}
+                                       </Typography>
+                                       <Typography>
+                                           {email.replace('@gmail.com', '')}
+                                       </Typography>
+                                   </Box>
+                               </CustomListItemButton>
+                           </Box>
+                   </ListItem>
+                   <Divider/>
                    <List>
-                       {menuItems.map(({label,Icon}) => (
-                           <ListItem key={label} onClick={handleCloseUserMenu}>
-                               <ListItemButton>
+                       {menuItems.map(({label,Icon, onClick}) => (
+                           <ListItem key={label} onClick={() => {
+                               handleCloseUserMenu();
+                               onClick()
+                           }}>
+                               <CustomListItemButton>
                                    <ListItemIcon>
                                        <Icon/>
                                    </ListItemIcon>
                                    <ListItemText primary={label}/>
-                               </ListItemButton>
+                               </CustomListItemButton>
                            </ListItem>
                        ))}
                    </List>
-
                </Menu>
            </>
     );
