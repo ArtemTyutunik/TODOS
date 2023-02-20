ba4a3ff0b5b80c5d72c8ff3ce9a7db2117df89a2
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-02-20 16:40:45 +0200

    Fixed checkbox in the DerailsCard component

--- src/pages/todos/pages/TodoDetailPage/TodoCardDetais.tsx
+++ src/pages/todos/pages/TodoDetailPage/TodoCardDetais.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootReducer} from '../../../../app/store';
 import {useParams} from 'react-router-dom';
 import BasicModal from '../../../../shared/components/modal';
 import DetailsCard from './DetailsCard';
+import {toggleTaskComplete} from '../../../../entities/todos/store/todo';
 
 const TodoDetailPage = () => {
   const {id} = useParams()
   const todos = useSelector((state: RootReducer) => state.todosReducer.todos);
+  const dispatch = useDispatch()
   const detailedTodo = todos.find( (todo) => todo.id === Number(id))
+
+  const onComplete = (e: React.SyntheticEvent) => {
+    e.stopPropagation();
+    dispatch(toggleTaskComplete(Number(id)));
+  };
 
   return (
     detailedTodo ?
     <BasicModal open={true} onClose={()=>{
       window.history.back()
     }}>
-      <DetailsCard todo={detailedTodo!}/>
+      <DetailsCard todo={detailedTodo!} onComplete={onComplete}/>
     </BasicModal> : null
   );
 };
