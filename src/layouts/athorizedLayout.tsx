a5b72259a84f34c5f1daae671943ad2ac36d1243
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-02-22 16:51:46 +0200

    fixed bug with checkbox color

--- src/layouts/athorizedLayout.tsx
+++ src/layouts/athorizedLayout.tsx
@@ -1,18 +1,23 @@
 import {Grid} from '@mui/material';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Drawer from '../entities/drawer';
 import Routing from '../pages/routes';
+import {ToastContainer} from 'react-toastify';
 
 const AuthorizedLayout= () => {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={3}>
-        <Drawer/>
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
+          <Drawer/>
+        </Grid>
+        <Grid item xs={6}>
+          <Routing/>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Routing/>
-      </Grid>
-    </Grid>
+      <ToastContainer/>
+    </>
   );
 };
 
