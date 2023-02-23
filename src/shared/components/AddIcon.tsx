8f5aaa52e318c0df13ef47189079fa094b40e456
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-02-23 14:17:04 +0200

    Replaced AddIcon with new component AddButtonIcon for better accessibility; Fix the problem when Add button and CreateTodoForm

---
+++ src/shared/components/AddIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+const AddButtonIcon = ({color}: {color:string}) => {
+  return (<AddIcon sx ={{color}}/>);
+};
+
+export default AddButtonIcon;
