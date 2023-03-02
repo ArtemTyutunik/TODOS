5e7c296686b5d184ed0c35c2bd5b3b79ab3fd462
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-03-02 21:12:20 +0200

    add to DueDateButton.tsx optional prop that vary its view(Standard or Outline)

---
+++ src/shared/components/DueDateComponents/DueDateButton.tsx
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import DropdownMenu from '@shared/components/dropdownMenu';
+import {Box, Button, Divider, Typography} from '@mui/material';
+import Calendar from './Calendar';
+import DueDateMenuList from './DueDateMenu';
+import {IDate} from '@shared/interfaces';
+import {TodoDescriptionStyles, TodoFlexboxStyles} from '@entities/todos/styles';
+import EventIcon from '@mui/icons-material/Event';
+import {overdueDate} from '@shared/constants';
+
+
+const buttonsStyles = {
+  maxWidth: '70px',
+  color: '#7d7b74',
+  backgroundColor: '#f5f5f5',
+  border: '1px solid #ddd',
+  boxShadow: 'none',
+  textTransform: 'none',
+  padding: '0 8px',
+  fontSize: '11px',
+};
+type DueDateButtonType = 'Standard' | 'Outline';
+
+interface Props {
+    date: IDate | undefined,
+    variant?: DueDateButtonType
+    onPassDateToBaseForm?: (date: IDate) => void
+}
+
+const DueDateButton = ({date, onPassDateToBaseForm, variant = 'Outline'}: Props) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const onSetDate = (date: IDate) => {
+    onPassDateToBaseForm && onPassDateToBaseForm(date);
+    handleCloseMenu();
+  };
+
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const isOverdue = overdueDate(date!);
+  return (
+    <>
+      { variant === 'Outline' ? (
+          <Button variant={'outlined'} sx={buttonsStyles} onClick={handleOpenMenu}>
+            {
+              date || 'Due date'
+            }
+          </Button>
+      ) : (
+          <Box sx = {{...TodoFlexboxStyles, cursor: 'pointer'}} mt={'5px'} color={isOverdue ? '#c40202' : '#808080'} onClick={handleOpenMenu}>
+            <EventIcon sx={{color: 'inherit', fontSize: '13px'}}/>
+            <Typography sx={{...TodoDescriptionStyles, marginLeft: '10px', paddingLeft: 0, fontSize: '13px', color: 'inherit'}}>
+              {
+                date || 'Due date'
+              }
+            </Typography>
+          </Box>
+          )
+      }
+
+      <DropdownMenu anchorEl={anchorEl} handleClose={handleCloseMenu}>
+        <Box display={'flex'} flexDirection={'column'} paddingTop={'5px'}>
+          <DueDateMenuList onSetDate={onSetDate}/>
+          <Divider/>
+          <Calendar onSetDate={onSetDate} initialDate={date}/>
+        </Box>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default DueDateButton;
