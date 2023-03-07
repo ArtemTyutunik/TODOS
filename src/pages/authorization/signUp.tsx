4176852940fa5b4527659647811ff8adc7ec5529
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-03-08 00:46:43 +0200

    add useLocalStorage hook to simplify work with local storage

--- src/pages/authorization/signUp.tsx
+++ src/pages/authorization/signUp.tsx
@@ -6,19 +6,21 @@ import {app} from '../../../firebaseConfig';
 
 import {signUpUser} from './store';
 import SignUpForm from '@entities/signUpForm/signUpForm';
+import useLocalStorage from '@shared/hooks/useLocalStorage';
 
 function SignUp() {
   const dispatch = useDispatch();
   const navigate = useNavigate()
+  const [, setValueLocalStorage] = useLocalStorage('user', null)
   const onSubmit = (data: IFormInputs) => signUpHandler(data.login, data.password);
 
   function signUpHandler(login: string, password: string) {
     const auth = getAuth(app);
     createUserWithEmailAndPassword(auth, login, password)
-        .then(({user})=> {
-          dispatch(signUpUser(user));
+        .then((response)=> {
+          dispatch(signUpUser(response));
           navigate('/today')
-          localStorage.setItem('user', JSON.stringify(user));
+          setValueLocalStorage(response);
         })
         .catch((e) => console.log(e));
   }
