28c456b6698a2bb01d45322c0926ed180868c515
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-03-16 21:51:28 +0200

    fix bug with url in unathorized layout

--- src/layouts/unathorizedLayout.tsx
+++ src/layouts/unathorizedLayout.tsx
@@ -1,12 +1,13 @@
 import Login from '@pages/authorization/login';
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import SignUp from '@pages/authorization/signUp';
 
 function UnathorizedLayout() {
   return (
     <Routes>
-      <Route path={'/'} element={<Login/>}/>
+      <Route path={'/login'} element={<Login/>}/>
       <Route path={'/sign-up'} element={<SignUp/>}/>
+      <Route path={'*'} element={<Navigate to={'/login'} replace={true}/>}/>
     </Routes>
 
   );
