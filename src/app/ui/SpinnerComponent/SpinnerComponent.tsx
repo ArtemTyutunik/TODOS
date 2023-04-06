4593cdaa2d3b48e94073d15fdea7d158335e81ea
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-04-06 23:07:31 +0300

    change view of spinner text

--- src/app/ui/SpinnerComponent/SpinnerComponent.tsx
+++ src/app/ui/SpinnerComponent/SpinnerComponent.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Box, Typography} from '@mui/material';
 
+
 import './SpinnerComponent.css'
 const SpinnerComponent = () => {
   return (
     <Box height={'100vh'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-      <div>
-        <Typography>
+      <Box display={'flex'} alignItems={'center'} flexDirection={'column'}>
+        <Typography color={'#1976d2'} fontSize={'45px'}>
                 Todos
         </Typography>
         <div className="custom-loader"></div>
-      </div>
+      </Box>
     </Box>
   );
 };
