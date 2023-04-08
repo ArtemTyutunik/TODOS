3d40b83651eac3844f7ba7a39357679183553298
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-04-08 16:02:53 +0300

    move logic of data fetching in separate custom hook

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,13 @@
-import React, {useEffect, useState} from 'react';
 import {ThemeProvider} from '@mui/material';
 import AppLayout from './ui/appLayout';
 import theme from '@app/theme';
 import {withStore} from './providers/withStore';
-import {useDispatch} from 'react-redux';
-import {fetchTasks} from '@entities/todos/store/todo';
 import SpinnerComponent from '@app/ui/SpinnerComponent/SpinnerComponent';
+import {useFetchAllTodos} from '@app/hooks/useFetchAllTodos';
 
 
 const App = () => {
-  const dispatch = useDispatch()
-  const [isFetching, setIsFetching] = useState(false)
-
-  useEffect(() => {
-    const fetchingData = () => new Promise((resolve) => {
-      setIsFetching(true)
-      fetch('http://localhost:4444/api/get_all')
-          .then((res) => res.json())
-          .then((res) => resolve(res))
-    })
-
-    fetchingData().then((res) => {
-      setIsFetching(false)
-      dispatch(fetchTasks(res))
-    })
-  }, [])
+  const [isFetching] = useFetchAllTodos();
 
   return (
     <ThemeProvider theme={theme}>
