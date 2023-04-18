57958aef7ec77efda0f7e12056b7d102fcc342d0
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-04-18 14:04:45 +0300

    move ui presentation into separate component

---
+++ src/entities/todos/components/DropdownActionMenu.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Box, Divider, IconButton, List, ListItemButton, Typography} from '@mui/material';
+import {PrioritiesFlags} from '@shared/components/PrioritiesFlags';
+import QueueIcon from '@mui/icons-material/Queue';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+interface DropdownActionMenuProps {
+    onDelete: (e: React.SyntheticEvent) => void,
+    onDuplicate: (e: React.SyntheticEvent) => void,
+    onSetPriority: (e: React.SyntheticEvent, priority: string) => void
+}
+
+const DropdownActionMenu = ({onDelete, onDuplicate, onSetPriority}: DropdownActionMenuProps) => {
+  return (
+    <Box padding={'0 10px'}>
+      <Box m={'10px 0'}>
+        <Typography>Set priority</Typography>
+        <Box display={'flex'}>
+          {PrioritiesFlags.map((Priority) => <Box mr={'10px'} key={Priority.value}>
+            <IconButton onClick={(event) => onSetPriority(event, Priority.value)}>
+              <Priority.Icon/>
+            </IconButton>
+          </Box>,
+          )}
+        </Box>
+      </Box>
+      <List>
+        <ListItemButton sx = {{padding: '10px 0', marginBottom: '10px'}} onClick={onDuplicate}>
+          <QueueIcon sx={{color: 'grey'}}/>
+          <Typography ml={'15px'}>
+                        Duplicate
+          </Typography>
+        </ListItemButton>
+      </List>
+
+      <Divider/>
+      <ListItemButton sx = {{padding: '10px 0', margin: '10px 0'}} onClick={onDelete}>
+        <DeleteIcon sx={{color: 'grey'}}/>
+        <Typography ml={'15px'}>
+                    Delete
+        </Typography>
+      </ListItemButton>
+    </Box>
+  );
+};
+
+export default DropdownActionMenu;
