10ae0ac52aa5bfc56097063c8eb2bc6f1112ba5a
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-04-19 00:33:39 +0300

    refactored sign up form

--- src/entities/signUpForm/signUpForm.tsx
+++ src/entities/signUpForm/signUpForm.tsx
@@ -1,47 +1,37 @@
 import React, {FC} from 'react';
-import {useForm, useFormState} from 'react-hook-form';
-import {Box, Button, Container, CssBaseline, Grid, Link, Typography} from '@mui/material';
+import {useForm} from 'react-hook-form';
+import {Box, Container, CssBaseline, Typography} from '@mui/material';
 import {IFormInputs, IInputsProps} from '@shared/forms/interfaces/interfaces';
-import {Link as RouterLink} from 'react-router-dom';
 import FormInputs from '@entities/signUpForm/FormInputs';
+import SubmitButton from '@shared/forms/authorization/SubmitButton';
+import FromLink from '@shared/forms/authorization/FormLink';
 
+const FormWrapperStyles = {
+  marginTop: 8,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}
 
 const SignUpForm:FC<IInputsProps> = ({onSubmit}) => {
   const {handleSubmit, control} = useForm<IFormInputs>({mode: 'onChange'});
-  const {isValid} = useFormState({control});
+
   return (
     <Container component="main" maxWidth="laptop">
       <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+      <Box sx={FormWrapperStyles}>
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
 
         <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{mt: 1}}>
           <FormInputs control={control}/>
-          <Button
-            type="submit"
-            fullWidth
-            disabled={!isValid}
-            variant="contained"
-            sx={{mt: 3, mb: 2}}
-          >
-              Sign Up
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link variant="body2" component = {RouterLink} to = '/'>
-                {'Have account? Login'}
-              </Link>
-            </Grid>
-          </Grid>
+          <SubmitButton control={control}>
+              Sign up
+          </SubmitButton>
+          <FromLink to={'/'}>
+            Have account? Login
+          </FromLink>
         </Box>
       </Box>
     </Container>
