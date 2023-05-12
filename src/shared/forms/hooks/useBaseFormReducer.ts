1c3835958a2ea60a9bf533df5dc2310d9773e6ee
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-05-12 22:52:07 +0300

    add base form reducer for easier state management

---
+++ src/shared/forms/hooks/useBaseFormReducer.ts
@@ -0,0 +1,33 @@
+import React from 'react';
+import {ITodo} from '@shared/interfaces';
+import {useReducer} from 'react';
+
+const initialStateFunction = (todo?: ITodo) => ({
+  id: todo?.id || Date.now(),
+  label: todo?.label || '',
+  description: todo?.description || '',
+  Tags: todo?.Tags || [],
+  date: todo?.date || '',
+  priority: todo?.priority || '4',
+  done: todo?.done || false,
+})
+
+interface formAction {
+  type: string;
+  payload: any;
+}
+
+const formReducer = (state: ITodo, action: formAction) => {
+  switch (action.type) {
+    case 'CHANGE_PRIORITY': return {...state, priority: action.payload}
+    default: return state
+  }
+}
+
+const useBaseFormReducer = (todo?: ITodo): [ITodo, React.Dispatch<formAction>] => {
+  const [state, dispatchAction] = useReducer(formReducer, initialStateFunction(todo));
+
+  return [state, dispatchAction]
+}
+
+export default useBaseFormReducer
